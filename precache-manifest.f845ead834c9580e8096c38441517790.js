8b511cc696cc834290d22b9c6042f9bbe5bb1c41
woojin624 <624jang@gmail.com>
2020-09-28 00:56:43 +0900

    Updates

---
+++ precache-manifest.f845ead834c9580e8096c38441517790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28f4636b3c5178e54b327cadb7a88bb",
+    "url": "/react_basic_habit_tracker/index.html"
+  },
+  {
+    "revision": "3f6267b1950782b34d0b",
+    "url": "/react_basic_habit_tracker/static/css/main.ee367655.chunk.css"
+  },
+  {
+    "revision": "ed45ae1a6102ce3bc176",
+    "url": "/react_basic_habit_tracker/static/js/2.b32eb186.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/react_basic_habit_tracker/static/js/2.b32eb186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6267b1950782b34d0b",
+    "url": "/react_basic_habit_tracker/static/js/main.e6f1c2e0.chunk.js"
+  },
+  {
+    "revision": "cce1584d664dd6fcc7a5",
+    "url": "/react_basic_habit_tracker/static/js/runtime-main.929c0c59.js"
+  }
+]);
